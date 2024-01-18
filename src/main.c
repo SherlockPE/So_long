b89c81a8546368ed9739c63e65bc821abca91aba
Fabricio <flopez-r@student.42madrid.com>
2024-01-18 13:59:41 +0100

    Checker map 1 completed and 2 in process

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 16:15:36 by flopez-r          #+#    #+#             */
-/*   Updated: 2024/01/17 18:22:14 by flopez-r         ###   ########.fr       */
+/*   Updated: 2024/01/18 13:57:47 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,58 +14,137 @@
 
 void	ft_perror(char *message)
 {
-	ft_printf("%s\n", message);
+	ft_printf("Error\n%s\n", message);
 	exit(EXIT_FAILURE);
 }
 
-int	get_alto(int fd)
-{
-	int	i;
-	char	*line;
-	
-	i = 0;
-	line = "";
-	while (line)
-	{
-		line = get_next_line(fd);
-		free(line);
-		i++;
-	}
-	return (i - 1);
-}
-
+//Function reads an archive and return a Matrix of the map
 char	**create_map(int fd)
 {
-	char	*line;
 	char	**matrix;
-	int		ancho;
 	int		alto;
+	int		i;
 
+	//Darle tamaño al mapa
+	alto = 32;
+	matrix = (char **)ft_calloc(alto + 1, sizeof(char *));
 
-	matrix = 0;
-	//Obtener el alto
-	alto = get_alto(fd);
-
-	//Obtener el ancho del mapa
-	ancho = 0;
-	line = get_next_line(fd);
-	while (line[ancho])
-		ancho++;
-	
-	ft_printf("Ancho: %d - Alto: %d", ancho, alto);
+	//Llenar el mapa
+	i = 0;
+	while (i < alto)
+	{
+		matrix[i] = get_next_line(fd);
+		i++;
+	}
 	return (matrix);
 }
 
+//																															Checker 1 (el mapa debe ser cuadrilatero)
+void	check_quadrilateral(char **map)
+{
+	int	i;
+	int	width;
+	int	first_width;
+
+	i = 0;
+	first_width = 0;
+
+	//Achar o primeiro comprimento
+	while (map[i][first_width] != '\n')
+		first_width++;
+
+	/* ft_printf("El ancho del mapa es: %d\n", first_width); */
+	//Verificar que o mapa é um quadrilátero
+	while (map[++i])
+	{
+		width = 0;
+		while (map[i][width] != '\n' && map[i][width])
+			width++;
+		/* ft_printf("Ancho conseguido en la posición %d: %d\n", i, width); */
+		if (width != first_width)
+			ft_perror("Map must be quadrilateral");
+	}
+}
+
+//																															Checker 2 (el mapa debe estar dentro de paredes)
+void	check_is_inside_walls(char	**map)
+{
+	int	i;
+	int	width;
+
+	//Achar o primeiro comprimento (1)
+	i = 0;
+	width = 0;
+	while (map[i][width] != '\n')
+	{
+		if (map[i][width] != '1')
+			ft_perror("Map must be inside walls");
+		width++;
+	}
+/* 	ft_printf("El ancho del mapa es: %d\n", width);
+	ft_printf("Ultima posicion de %d: %c\n", i, map[i][width - 1]); */
+	
+	
+	//Verificar as beiras (2)
+	while (map[i])
+	{
+		if (map[i][0] != '1' || map[i][width - 1] != '1')
+			ft_perror("Map must be inside walls");
+		i++;
+	}
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+}
+
+//Function checks if a map is valid to play
 void	check_map(char *map)
 {
-	// char	**matrix;
+	char	**matrix;
 	int		fd;
 
 	fd = open(map, O_RDONLY);
-	// matrix = create_map(fd);
-	ft_printf("La altura es: %d\n", get_alto(fd));
 
+	//Get map
+	matrix = create_map(fd);
+
+	//Imprimir mapa
+	int	i = 0;
+	while (matrix[i])
+	{
+		ft_printf("%s", matrix[i]);
+		i++;
+	}
+	ft_printf("\n");
+
+	//Checkers
+	check_quadrilateral(matrix);//Done
+	check_is_inside_walls(matrix);
+	
 }
+
+
 
 int	main(int argc, char *argv[])
 {
