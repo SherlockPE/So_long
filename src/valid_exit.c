0d39f97f3d3224af96852eb585e90a410722fbd5
Fabricio <flopez-r@student.42madrid.com>
2024-01-22 18:24:57 +0100

    Check valid exit finallly done... or not?

--- src/valid_exit.c
+++ src/valid_exit.c
@@ -6,7 +6,7 @@
 /*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 18:37:38 by flopez-r          #+#    #+#             */
-/*   Updated: 2024/01/22 17:43:59 by flopez-r         ###   ########.fr       */
+/*   Updated: 2024/01/22 18:15:39 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,43 +34,73 @@ int	count_collectibles(char **map, int *col)
 	return (*col);
 }
 
-//Function Discount the value of collectibles and END
-void	discount_values(char actual_pos, int *col, int *end)
-{
-	if (actual_pos == 'C')
-		(*col)--;
-	else if (actual_pos == 'E')
-		(*end)--;
-	if (*col == 0 && *end == 0)
-		return ;
-	// ft_perror("El mapa no es jugable ⛹️‍♂️");
-}
+// //Function Discount the value of collectibles and END
+// void	discount_values(char actual_pos, int *col, int *end)
+// {
+// 	if (actual_pos == 'C')
+// 		(*col)--;
+// 	else if (actual_pos == 'E')
+// 		(*end)--;
+// 	if (*col == 0 && *end == 0)
+// 		return ;
+// 	// ft_perror("El mapa no es jugable ⛹️‍♂️");
+// }
 
 //Function looks for "0, E, C" characters and transforms to a P
-int	expand_players(char **map, int i, int j)
+int	expand_players(char **map, int i, int j, int *col)
 {
 	char	actual;
 
 	actual = map[i][j];
-	if (actual == '1')
+	if (actual == '1' || actual == 'E' || actual == 'P')
 		return (0);
-	//Down
-	if (map[i + 1][j] == 'P')
+	//Down and Left
+	if (map[i + 1][j] == 'P' || map[i][j - 1] == 'P')
 		map[i][j] = 'P';
-	//Left
-	else if (map[i][j - 1] == 'P')
+	//Right and Up
+	else if (map[i][j + 1] == 'P' || map[i - 1][j] == 'P')
 		map[i][j] = 'P';
-	//Right
-	else if (map[i][j + 1] == 'P')
-		map[i][j] = 'P';
-	//Up
-	else if (map[i - 1][j] == 'P')
-		map[i][j] = 'P';
+	//If Expands
 	if (map[i][j] == 'P')
+	{
+		//If collectiona
+		if (actual == 'C')
+			(*col)--;
 		return (1);
+	}
 	return (0);
 }
 
+int	check_exit_corners(char **map, int *end)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (map[i])
+	{
+		j = 1;
+		while (map[i][j])
+		{
+			if (map[i][j] == 'E')
+			{
+				if (map[i + 1][j] == 'P' || map[i][j - 1] == 'P')
+				{
+					(*end)--;
+					return (0);
+				}
+				else if (map[i][j + 1] == 'P' || map[i - 1][j] == 'P')
+				{
+					(*end)--;
+					return (0);
+				}
+			}
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
 
 //Function replicates the character P if they found around a P character
 void	look_for_players(char **map, int *col, int *end)
@@ -78,10 +108,9 @@ void	look_for_players(char **map, int *col, int *end)
 	int		i;
 	int		j;
 	char	cycle;
-	char	actual;
 
 	cycle = 1;
-	while (cycle)
+	while (cycle && ((*col) || check_exit_corners(map, end)))
 	{
 		cycle = 0;
 		i = 1;
@@ -90,19 +119,17 @@ void	look_for_players(char **map, int *col, int *end)
 			j = 1;
 			while (map[i][j])
 			{
-				actual = ft_toupper(map[i][j]);
-				// if (discount_values(actual, col, end))
-				// 	return ;
-				if (expand_players(map, i, j))
+				map[i][j] = ft_toupper(map[i][j]);
+				if (expand_players(map, i, j, col))
 					cycle = 1;
-				else
-					discount_values(actual, col, end);
 				j++;
 			}
+			// print_map(map);
 			i++;
 		}
 	}
 }
+
 
 //	Checker 4 (El mapa debe ser jugable)
 void	check_valid_exit(char **map)
@@ -115,14 +142,21 @@ void	check_valid_exit(char **map)
 	//Count how many collectionables are in the map
 	count_collectibles(map, &col);
 	ft_printf("Cantidad de coleccionables: %d\n", col);
-	
+
+	//Print map
 	print_map(map);
+
 	//Expand player if they are in a "0, C, E"
 	map_copy = map;
 	look_for_players(map_copy, &col, &end);
+
+	//Check if the path is valid
 	if (col != 0 || end != 0)
 		ft_perror("El mapa no es jugable ⛹️‍♂️");
 
+	//Print map
 	print_map(map);
+
+	//Print cant od collectibles
 	ft_printf("Cantidad de coleccionables: %d\n", col);
 }
