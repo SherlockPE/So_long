333b86d43a220010c9c41ed876ebea6bb98612d3
Fabricio <flopez-r@student.42madrid.com>
2024-02-01 14:18:45 +0100

    God part

--- main.c
+++ main.c
@@ -6,95 +6,99 @@
 /*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 13:08:00 by flopez-r          #+#    #+#             */
-/*   Updated: 2024/02/01 12:23:29 by flopez-r         ###   ########.fr       */
+/*   Updated: 2024/02/01 14:17:04 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pruebas.h"
 
-typedef struct s_data
+void	set_background(t_data 	*data)
 {
-	void	*mlx;
-	void	*win;
-
-	int		x_player;
-	int		y_player;
-}			t_data;
-
-typedef struct s_img_dta
-{
-	void	*player;
-	void	*wall;
-	void	*floor;
-}			t_img_data;
-
-void	put_image(t_data *data, int x, int y)
-{
-	mlx_put_image_to_window(data->mlx, data->win, data->img_wall, x
-			* PIXEL_SIZE, y * PIXEL_SIZE);
-}
-
-int	key_event(int keycode, t_data *data)
-{
-	printf("Key code: %d\n", keycode);
-	if (keycode == 0 || keycode == 124)
-	{
-		data->x_player++;
-		put_image(data, data->x_player, data->y_player);
-	}
-	return (0);
-}
-
-void	put_background(t_data *data)
-{
-	int i;
-	int j;
+	int	i;
+	int	j;
 
 	i = 7;
-	while (i)
+	while (i--)
 	{
 		j = 7;
-		while (j)
+		while (j--)
 		{
-			
+			mlx_put_image_to_window(data->mlx, data->win, data->floor, j * PIXEL_SIZE, i * PIXEL_SIZE);
 		}
-		
 	}
 }
 
-void	get_images(t_data *data, t_img_data *img_data)
+void	get_images(t_data *data)
 {
 	int	width;
-	int	heigth;
-
-	img_data->floor = mlx_xpm_file_to_image(data->mlx, "floor.xpm", &width,
-			&heigth);
-	img_data->player = mlx_xpm_file_to_image(data->mlx, "player.xpm", &width,
-			&heigth);
-
-	if (!img_data->floor || !img_data->player)
+	int	height;
+	data->player = mlx_xpm_file_to_image(data->mlx, "./textures/player.xpm", &width, &height);
+	data->walls = mlx_xpm_file_to_image(data->mlx, "./textures/wall.xpm", &width, &height);
+	data->floor = mlx_xpm_file_to_image(data->mlx, "./textures/floor.xpm", &width, &height);
+	if (!data->player || !data->walls || !data->floor)
 	{
-		printf("Imagen no valida/encontrada!\n");
-		return (1);
+		printf("Fallo al crear una imagen");
+		exit(1);
 	}
 }
+
+
+void	put_player(t_data *data)
+{
+	mlx_put_image_to_window(data->mlx, data->win, data->player, data->x_cords * PIXEL_SIZE, data->y_cords * PIXEL_SIZE);
+}
+
+
+void	reset_player(t_data *data)
+{
+	mlx_clear_window(data->mlx, data->win);
+	set_background(data);
+	put_player(data);
+}
+
+void	end_program(t_data *data)
+{
+	mlx_destroy_window(data->mlx, data->win);
+	// mlx_destroy_image(data->mlx, data->player);
+}
+
+int	move_player(int keycode, t_data *data)
+{
+	printf("Keycode %d\n", keycode);
+	if (keycode == UP_L || keycode == UP_D)//Up
+		data->y_cords -= 1;
+	else if (keycode == LEFT_L || keycode == LEFT_D)//Left
+		data->x_cords--;
+	else if (keycode == DOWN_L || keycode == DOWN_D)//Down
+		data->y_cords++;
+	else if (keycode == RIGHT_L || keycode == RIGHT_D)//Right
+		data->x_cords++;
+	else if (keycode == ESC)
+		end_program(data);
+	reset_player(data);
+	return (1);
+}
+
 
 int	main(void)
 {
-	t_data		data;
-	t_img_data	img_data;
-	int			width;
-	int			heigth;
+	t_data	data;
 
-	data.mlx = mlx_init();
-	data.win = mlx_new_window(data.mlx, 448, 448, "Hola, esto es una prueba");
-	get_images(&data, &img_data);
+	data.mlx = mlx_init(data.mlx);
+	data.win = mlx_new_window(data.mlx, 7 * PIXEL_SIZE, 7 * PIXEL_SIZE, "Prueba de movimiento");
+	
+	get_images(&data);
 
-	put_background(&img_data);
-	data.x_player = 0;
-	data.y_player = 0;
-	put_image(&data, 0, 0);
-	mlx_key_hook(data.win, key_event, &data);
+	set_background(&data);
+
+	//Set cords
+	data.x_cords = 0;
+	data.y_cords = 0;
+	
+	put_player(&data);
+	mlx_key_hook(data.win, move_player, &data);
+
+
+
 	mlx_loop(data.mlx);
-	return (0);
 }
--- pruebas.h
+++ pruebas.h
@@ -6,19 +6,74 @@
 /*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 13:09:14 by flopez-r          #+#    #+#             */
-/*   Updated: 2024/02/01 11:17:07 by flopez-r         ###   ########.fr       */
+/*   Updated: 2024/02/01 14:13:46 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PRUEBAS_H
 # define PRUEBAS_H
 
-# include <mlx.h>
-
 # ifndef PIXEL_SIZE
 #  define PIXEL_SIZE 64
 # endif
 
+//ESQ
+# ifndef ESC
+#  define ESC 53
+# endif
+
+//WASD
+# ifndef UP_L
+#  define UP_L 13
+# endif
+
+# ifndef LEFT_L
+#  define LEFT_L 0
+# endif
+
+# ifndef DOWN_L
+#  define DOWN_L 1
+# endif
+
+# ifndef RIGHT_L
+#  define RIGHT_L 2
+# endif
+
+//ARROWS
+# ifndef UP_D
+#  define UP_D 126
+# endif
+
+# ifndef LEFT_D
+#  define LEFT_D 123
+# endif
+
+# ifndef DOWN_D
+#  define DOWN_D 125
+# endif
+
+# ifndef RIGHT_D
+#  define RIGHT_D 124
+# endif
+
+# include <mlx.h>
 # include <stdio.h>
+# include <stdlib.h>
+
+typedef struct s_data
+{
+	void	*mlx;
+	void	*win;
+
+	void	*player;
+	void	*walls;
+	void	*floor;
+
+
+	int		x_cords;
+	int		y_cords;
+}			t_data;
+
+
 
 #endif
